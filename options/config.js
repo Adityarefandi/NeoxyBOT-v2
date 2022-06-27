7ea442ed6ea0eee2874a8d918fbb98f0a772a3f8
Adityarefandi <108258298+Adityarefandi@users.noreply.github.com>
2022-06-27 20:37:21 +0700

    Update config.js

--- options/config.js
+++ options/config.js
@@ -11,19 +11,19 @@ global.thum = fs.readFileSync("./plugins/media/image/thumb.jpg")
 global.log0 = fs.readFileSync("./plugins/media/image/thumb.jpg")
 global.err4r = fs.readFileSync("./plugins/media/image/thumb.jpg")
 global.locimg = fs.readFileSync("./plugins/media/image/loc.jpg")
-global.owner = ['6283834558105', '6285789004732']
-global.numberOwner = "6283834558105"
+global.owner = ['6282244877433', '6282244877433']
+global.numberOwner = "6282244877433"
 global.group = "https://chat.whatsapp.com/E3zewfxrc5pKE6Rzb3BuqG"
 global.rkyt = []
-global.banUser = ['6281366679021@s.whatsapp.net']
+global.banUser = ['6282244877433@s.whatsapp.net']
 global.ntilink = ['120363043965529911@g.us','120363022988111568@g.us']
 global.wlcm = ['120363043965529911@g.us','120363023112413670@g.us','19054091487-1424650263@g.us','120363022988111568@g.us']
 global.gcrevoke = ['19054091487-1424650263@g.us']
-global.packname = "Neoxy Bot"
-global.author = "Lexxy Official"
+global.packname = "Ditz-bot"
+global.author = "Aditya Refandi Official"
 global.sessionName = "session"
-global.footer = "© Created By Lexxy Official"
-global.linkgrupss = "https://chat.whatsapp.com/E3zewfxrc5pKE6Rzb3BuqG"
+global.footer = "© Created By Aditya Refandi Official"
+global.linkgrupss = "https://chat.whatsapp.com/BTOjwo2QW3TESrV3XWcJLa"
 global.antitags = true
 global.txtSewabot = "1-Minggu : Rp5.000\n2-Minggu : Rp.10.000\n1-Bulan : Rp.20.000\n2-Bulan : Rp.30.000\n3-Bulan : Rp40.000"
 global.prefa = ['','!','.','🐦','🐤','🗿']
@@ -36,12 +36,12 @@ global.mess = {
     private: 'Fitur Digunakan Hanya Untuk Private Chat!',
     bot: 'Fitur Khusus Pengguna Nomor Bot',
     wait: 'Loading...',
-    linkm: 'Linknya Mana Kak?',
-    ban: 'Kamu telah dibanned oleh owner, Jika ingin di unbanned chat owner'
+    linkm: 'Linknya Mana Deck?',
+    ban: ' Awokawokaowok mampus di Ban, Jika ingin di unbanned chat owner'
 }
 global.limitawal = {
     rakyat: "Infinity",
-    free: 100
+    free: 15
 }
 
 let file = require.resolve(__filename)
